75aee149138f8ae2a4967cce7d5aee2e603d4e38
msnatt <nattawat.mue.63@ubu.ac.th>
2026-03-04 10:58:05 +0700

    darkmode function

--- components/header.php
+++ components/header.php
@@ -5,24 +5,30 @@ $currentTime = date('H:i:s');
 ?>
 
 <!-- Header -->
-<header class="flex items-center justify-between p-[0.5vw] border-b border-stone-200 bg-white shrink-0">
+<header class="flex items-center justify-between p-[0.5vw] border-b border-stone-200 bg-white shrink-0 dark:bg-slate-900 dark:border-slate-800">
     <div class="flex items-center gap-3">
-        <?php include 'navbar.php'; ?>
-        <div class="size-9 w-[4rem] h-[4rem] bg-[#FF8021] rounded-xl flex items-center justify-center text-white shadow-sm shadow-primary/20">
-            <span class="emojione-monotone--chicken text-2xl text-white"></span>
+        
+        <div class="w-[4rem] h-[4rem] rounded-xl flex items-center justify-center shadow-sm dark:text-white dark:bg-canvas-dark">
+            <span class="ic--twotone-hub text-2xl"></span>
         </div>
         <div>
-            <h1 class="text-[#1d130c] text-[1.5vw] font-bold leading-none">
+            <h1 class="text-[#1d130c] dark:text-white text-[1.5vw] font-bold leading-none">
                 <?php echo isset($pageTitle) ? $pageTitle : "Not Set Name in param \$pageTitle"; ?>
             </h1>
-            <p class="text-[0.75vw] text-stone-500 font-medium uppercase tracking-wider mt-0.5"><?php echo isset($subTitle) ? $subTitle : "---" ?></p>
+            <p class="text-[0.75vw] text-stone-500 dark:text-stone-400 font-medium uppercase tracking-wider mt-0.5"><?php echo isset($subTitle) ? $subTitle : "---" ?></p>
         </div>
     </div>
     <div class="flex items-center gap-4">
+        <div id="theme-toggle-container" onclick="document.documentElement.classList.toggle('dark')">
+            <div id="theme-toggle-thumb">
+                <span class="material-icons-round text-[14px] text-orange-500 block dark:hidden">wb_sunny</span>
+                <span class="material-icons-round text-[14px] text-slate-700 hidden dark:block">dark_mode</span>
+            </div>
+        </div>
         <div class="flex flex-col items-end border-l border-stone-200 pl-4">
-            <span class="text-[0.75vw] font-bold text-stone-400 uppercase tracking-widest leading-none mb-0.5">อัปเดตล่าสุด</span>
-            <span class="text-[0.75vw] font-bold text-stone-600 text-center" id="last-update"><?php echo $currentTime; ?></span>
-            <span class="text-[0.75vw] text-stone-600 font-bold leading-none" id="start-date"> -- --- ---- </span>
+            <span class="text-[0.75vw] font-bold text-stone-400 dark:text-stone-200 uppercase tracking-widest leading-none mb-0.5">อัปเดตล่าสุด</span>
+            <span class="text-[0.75vw] font-bold text-stone-600 dark:text-stone-300 text-center" id="last-update"><?php echo $currentTime; ?></span>
+            <span class="text-[0.75vw] text-stone-600 dark:text-stone-300 font-bold leading-none" id="start-date"> -- --- ---- </span>
         </div>
     </div>
 </header>
--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,7 @@ $subTitle = "Realtime Control Project";
 ?>
 
 <!DOCTYPE html>
-<html class="light" lang="th">
+<html class="dark" lang="th">
 <?php include("../scripts/ref.html"); ?>
 <?php include("../styles/css-default.html"); ?>
 <?php include("../styles/css-icon.html"); ?>
@@ -22,122 +22,133 @@ $subTitle = "Realtime Control Project";
         <div class="feedback-line"></div>
 
         <div class="absolute right-[42.01%] top-[63%]">
-            <div class="w-[8vw] bg-blue-50 border-2 border-blue-200 rounded-2xl p-4 shadow-lg">
+            <div class="w-[8vw] bg-blue-50 dark:bg-blue-950 border-2 border-blue-200 rounded-2xl p-4 shadow-lg">
                 <div class="flex items-center justify-between mb-2">
-                    <span class="text-[10px] font-bold text-blue-500 dark:text-white-400 uppercase tracking-tight">PROCESS (PV)</span>
+                    <span class="text-[10px] font-bold text-blue-500 dark:text-blue-50 uppercase tracking-tight">PROCESS (PV)</span>
                     <span class="material-icons-outlined text-blue-400 text-sm">track_changes</span>
                 </div>
-                <div id="sv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0</div>
+                <div id="sv" class="text-3xl font-mono font-bold text-blue-950 dark:text-white">0</div>
             </div>
         </div>
 
         <div class="flex items-center w-full p-[3vw]">
             <!-- ==== SETPOINT (SP) ==== -->
-            <div class="w-[8vw] bg-red-50 dark:bg-red-950/20 border-2 border-red-200 dark:border-red-900/50 rounded-2xl p-4 shadow-lg">
+            <div class="w-[8vw] bg-red-50 dark:bg-red-950 border-2 border-red-200 dark:border-red-900 rounded-2xl p-4 shadow-lg">
                 <div class="flex items-center justify-between mb-2">
-                    <span class="text-[10px] font-bold text-black-500 dark:text-white-400 uppercase tracking-tight">Setpoint (SP)</span>
+                    <span class="text-[10px] font-bold text-red-500 dark:text-rose-100 uppercase tracking-tight">Setpoint (SP)</span>
                     <span class="material-icons-outlined text-red-400 text-sm">track_changes</span>
                 </div>
-                <div id="sp" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">30.24</div>
+                <div id="sp" class="text-3xl font-mono font-bold text-red-600 dark:text-white">30.24</div>
             </div>
             <div class="arrow-line flex-1"></div>
             <div class="w-[2vw] h-[2vw] rounded-full border-2 text-[2vw] flex justify-center items-center border border-slate-300 shadow-sm z-10">
-                <span class="text-xs font-bold">Σ</span>
+                <span class="text-xs font-bold dark:text-white">Σ</span>
             </div>
             <div class="arrow-line flex-1"></div>
             <!-- ==== Error ==== -->
-            <div class="w-[8vw] bg-orange-50 dark:bg-orange-950/20 border-2 border-orange-200 dark:border-orange-900/50 rounded-2xl p-4 shadow-lg">
+            <div class="w-[8vw] bg-orange-50 dark:bg-orange-950 border-2 border-orange-200 dark:border-orange-900 rounded-2xl p-4 shadow-lg">
                 <div class="flex items-center justify-between mb-2">
-                    <span class="text-[10px] font-bold text-orange-500 dark:text-orange-400 uppercase tracking-tight">Error (ERR)</span>
+                    <span class="text-[10px] font-bold text-orange-500 dark:text-orange-100 uppercase tracking-tight">Error (ERR)</span>
                     <span class="material-icons-outlined text-orange-400 text-sm">warning_amber</span>
                 </div>
-                <div id="error" class="text-3xl font-mono font-bold text-orange-600 dark:text-orange-300">157.24</div>
+                <div id="error" class="text-3xl font-mono font-bold text-orange-600 dark:text-white">157.24</div>
             </div>
             <div class="arrow-line flex-1"></div>
             <div class="min-h-[40vh] flex gap-[0.25vw]">
                 <div class="flex flex-col justify-between">
                     <!-- ==== KP ==== -->
-                    <div class="w-[8vw] bg-white dark:bg-red-950/20 border-2 border-slate-200 dark:border-red-900/50 rounded-2xl p-4 shadow-lg">
+                    <div class="w-[8vw] bg-white dark:bg-slate-950 border-2 border-slate-200 dark:border-stone-200 rounded-2xl p-4 shadow-lg">
                         <div class="flex items-center justify-between mb-2">
-                            <span class="text-[10px] font-bold text-black-500 dark:text-white-400 uppercase tracking-tight">KP (PROP)</span>
+                            <span class="text-[10px] font-bold text-black-500 dark:text-stone-200 uppercase tracking-tight">KP (PROP)</span>
                             <span class="material-icons-outlined text-slate-400 text-sm">track_changes</span>
                         </div>
-                        <div id="kp" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0.00</div>
+                        <div id="kp" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
                     </div>
                     <!-- ==== KI ==== -->
-                    <div class="w-[8vw] bg-white dark:bg-red-950/20 border-2 border-slate-200 dark:border-red-900/50 rounded-2xl p-4 shadow-lg">
+                    <div class="w-[8vw] bg-white dark:bg-slate-950 border-2 border-slate-200 dark:border-stone-200 rounded-2xl p-4 shadow-lg">
                         <div class="flex items-center justify-between mb-2">
-                            <span class="text-[10px] font-bold text-black-500 dark:text-white-400 uppercase tracking-tight">KI (INTEG)</span>
+                            <span class="text-[10px] font-bold text-black-500 dark:text-stone-200 uppercase tracking-tight">KI (INTEG)</span>
                             <span class="material-icons-outlined text-slate-400 text-sm">track_changes</span>
                         </div>
-                        <div id="ki" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0.00</div>
+                        <div id="ki" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
                     </div>
                     <!-- ==== KD ==== -->
-                    <div class="w-[8vw] bg-white dark:bg-red-950/20 border-2 border-slate-200 dark:border-red-900/50 rounded-2xl p-4 shadow-lg">
+                    <div class="w-[8vw] bg-white dark:bg-slate-950 border-2 border-slate-200 dark:border-stone-200 rounded-2xl p-4 shadow-lg">
                         <div class="flex items-center justify-between mb-2">
-                            <span class="text-[10px] font-bold text-black-500 dark:text-white-400 uppercase tracking-tight">KD (DERIV)</span>
+                            <span class="text-[10px] font-bold text-black-500 dark:text-stone-200 uppercase tracking-tight">KD (DERIV)</span>
                             <span class="material-icons-outlined text-slate-400 text-sm">track_changes</span>
                         </div>
-                        <div id="kd" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0.00</div>
+                        <div id="kd" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
                     </div>
                 </div>
                 <div class="flex flex-col justify-between">
                     <div class="w-[5vw] max-h-[100px] flex items-center">
                         <div class="arrow-line flex-1"></div>
-                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
-                            <div id="multi-kp-1" class="text-[0.75vw]">1</div>
-                            <div id="multi-kp-10" class="text-[0.75vw]">10</div>
-                            <div id="multi-kp-100" class="text-[0.75vw]">100</div>
+                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white dark:bg-slate-950 border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
+                            <div id="multi-kp-1" class="text-[0.75vw] dark:text-white">1</div>
+                            <div id="multi-kp-10" class="text-[0.75vw] dark:text-white">10</div>
+                            <div id="multi-kp-100" class="text-[0.75vw] dark:text-white">100</div>
                         </div>
                         <div class="arrow-line flex-1"></div>
                     </div>
                     <div class="w-[5vw] max-h-[100px] flex items-center">
                         <div class="arrow-line flex-1"></div>
-                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
-                            <div id="multi-ki-1" class="text-[0.75vw]">1</div>
-                            <div id="multi-ki-10" class="text-[0.75vw]">10</div>
-                            <div id="multi-ki-100" class="text-[0.75vw]">100</div>
+                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white dark:bg-slate-950 border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
+                            <div id="multi-ki-1" class="text-[0.75vw] dark:text-white">1</div>
+                            <div id="multi-ki-10" class="text-[0.75vw] dark:text-white">10</div>
+                            <div id="multi-ki-100" class="text-[0.75vw] dark:text-white">100</div>
                         </div>
                         <div class="arrow-line flex-1"></div>
                     </div>
                     <div class="w-[5vw] max-h-[100px] flex items-center">
                         <div class="arrow-line flex-1"></div>
-                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
-                            <div id="multi-kd-1" class="text-[0.75vw]">1</div>
-                            <div id="multi-kd-10" class="text-[0.75vw]">10</div>
-                            <div id="multi-kd-100" class="text-[0.75vw]">100</div>
+                        <div class="w-[3vw] h-[100px] flex flex-col justify-between items-center bg-white dark:bg-slate-950 border-2 border-slate-200 rounded-2xl p-[0.5vw] shadow-lg">
+                            <div id="multi-kd-1" class="text-[0.75vw] dark:text-white">1</div>
+                            <div id="multi-kd-10" class="text-[0.75vw] dark:text-white">10</div>
+                            <div id="multi-kd-100" class="text-[0.75vw] dark:text-white">100</div>
                         </div>
                         <div class="arrow-line flex-1"></div>
                     </div>
                 </div>
-                <div class="w-[30vw] bg-white border-2  border-slate-200 p-[0.5vw] rounded-2xl shadow-lg">
+                <div class="w-[30vw] bg-white dark:bg-slate-950 border-2 border-slate-200 dark:border-slate-200 p-[0.5vw] rounded-2xl shadow-lg">
 
                     <canvas id="chart"></canvas>
                 </div>
             </div>
             <div class="arrow-line flex-1"></div>
-            <div class="w-[8vw] bg-blue-50 border-2 border-blue-200 rounded-2xl p-4 shadow-lg">
+            <!-- <div class="w-[2vw] h-[2vw] relative text-[2vw] flex justify-center items-center shadow-sm z-10">
+                <span class="text-xs font-bold">Output</span> -->
+            <!-- ==== MV ==== -->
+            <div class="w-[8vw] bg-green-50 dark:bg-green-950 border-2 border-green-200 dark:border-green-900 rounded-2xl p-4 shadow-lg">
+                <div class="flex items-center justify-between mb-2">
+                    <span class="text-[10px] font-bold text-green-500 dark:text-white-400 uppercase tracking-tight">MV</span>
+                    <span class="material-icons-outlined text-green-400 text-sm">track_changes</span>
+                </div>
+                <div id="mv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
+            </div>
+            <!-- ==== PV ==== -->
+            <!-- <div class="w-[8vw] bg-blue-50 border-2 border-blue-200 rounded-2xl p-4 shadow-lg">
                 <div class="flex items-center justify-between mb-2">
                     <span class="text-[10px] font-bold text-blue-500 dark:text-white-400 uppercase tracking-tight">PROCESS (PV)</span>
                     <span class="material-icons-outlined text-blue-400 text-sm">track_changes</span>
                 </div>
-                <div id="pv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0.00</div>
-            </div>
+                <div id="pv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
+            </div> -->
             <div class="arrow-line flex-1"></div>
             <div class="w-[2vw] h-[2vw] rounded-full border-2 text-[2vw] flex justify-center items-center border border-slate-300 shadow-sm z-10">
-                <span class="text-xs font-bold">Σ</span>
+                <span class="text-xs font-bold dark:text-white">Σ</span>
             </div>
             <div class="arrow-line flex-1"></div>
             <div class="w-[2vw] h-[2vw] relative text-[2vw] flex justify-center items-center shadow-sm z-10">
-                <span class="text-xs font-bold">Output</span>
+                <span class="text-[0.5vw] font-bold dark:text-white">Output</span>
                 <!-- ==== MV ==== -->
-                <div class="absolute bottom-[10vh] w-[8vw] border-2 bg-green-50 border-green-200 dark:border-green-900/50 rounded-2xl p-4 shadow-lg">
+                <!-- <div class="absolute bottom-[10vh] w-[8vw] border-2 bg-green-50 border-green-200 dark:border-green-900/50 rounded-2xl p-4 shadow-lg">
                     <div class="flex items-center justify-between mb-2">
                         <span class="text-[10px] font-bold text-green-500 dark:text-white-400 uppercase tracking-tight">MV</span>
                         <span class="material-icons-outlined text-green-400 text-sm">track_changes</span>
                     </div>
-                    <div id="mv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white-300">0.00</div>
-                </div>
+                    <div id="mv" class="text-3xl font-mono font-bold text-stone-600 dark:text-white">0.00</div>
+                </div> -->
             </div>
             <div class="arrow-line flex-1"></div>
 
@@ -146,37 +157,37 @@ $subTitle = "Realtime Control Project";
             <div class="bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-xl p-4 shadow-lg min-w-[280px]">
                 <div class="flex items-center justify-between mb-3">
                     <div class="flex items-center gap-2">
-                        <span class="material-icons-outlined text-sm text-slate-400">sensors</span>
-                        <span class="text-[10px] font-bold uppercase tracking-wider text-slate-500">Connectivity</span>
+                        <span class="material-icons-outlined text-sm text-slate-400 dark:text-white">sensors</span>
+                        <span class="text-[10px] font-bold uppercase tracking-wider text-slate-500 dark:text-white">Connectivity</span>
                     </div>
                     <div class="flex items-center gap-1">
                         <div class="w-2 h-2 rounded-full bg-green-500"></div>
-                        <span class="text-[10px] font-medium text-slate-400">Connected</span>
+                        <span class="text-[10px] font-medium text-slate-400 dark:text-white">Connected</span>
                     </div>
                 </div>
                 <div class="grid grid-cols-2 gap-2 mb-3">
                     <div class="bg-slate-50 dark:bg-slate-800/50 rounded-lg p-2 border border-slate-100 dark:border-slate-800 flex items-center justify-between">
-                        <span class="text-xs font-mono">IOT</span>
-                        <span class="w-1.5 h-1.5 rounded-full bg-green-400"></span>
+                        <span class="text-xs font-mono dark:text-white">IOT</span>
+                        <span class="w-1.5 h-1.5 rounded-full bg-green-300 dark:text-white"></span>
                     </div>
                     <div class="bg-slate-50 dark:bg-slate-800/50 rounded-lg p-2 border border-slate-100 dark:border-slate-800 flex items-center justify-between">
-                        <span class="text-xs font-mono">USB</span>
-                        <span class="w-1.5 h-1.5 rounded-full bg-slate-300"></span>
+                        <span class="text-xs font-mono dark:text-white">USB</span>
+                        <span class="w-1.5 h-1.5 rounded-full bg-slate-300 dark:bg-white"></span>
                     </div>
                 </div>
-                <button class="w-full py-2 bg-slate-100 dark:bg-slate-800 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-lg text-xs font-bold uppercase flex items-center justify-center gap-2 transition-colors">
-                    <span class="material-icons-outlined text-sm">wifi_off</span>
+                <button class="w-full py-2 bg-slate-100 dark:bg-slate-800 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-lg text-xs dark:text-white font-bold uppercase flex items-center justify-center gap-2 transition-colors">
+                    <span class="material-icons-outlined text-sm dark:text-white">wifi_off</span>
                     Reset Wifi ESP32
                 </button>
             </div>
         </div>
         <div class="absolute bottom-6 right-6 flex flex-col gap-2">
             <button class="flex items-center justify-between gap-4 px-4 py-2.5 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-lg shadow-md hover:translate-y-[-2px] transition-transform">
-                <span class="text-xs font-bold text-slate-600 dark:text-slate-400">CSV Report</span>
+                <span class="text-xs font-bold text-slate-600 dark:text-slate-200">CSV Report</span>
                 <span class="material-icons-outlined text-blue-500 text-lg">description</span>
             </button>
             <button class="flex items-center justify-between gap-4 px-4 py-2.5 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-lg shadow-md hover:translate-y-[-2px] transition-transform">
-                <span class="text-xs font-bold text-slate-600 dark:text-slate-400">Excel Data</span>
+                <span class="text-xs font-bold text-slate-600 dark:text-slate-200">Excel Data</span>
                 <span class="material-icons-outlined text-green-500 text-lg">grid_on</span>
             </button>
         </div>
@@ -185,6 +196,8 @@ $subTitle = "Realtime Control Project";
     <?php include "../components/footer.php"; ?>
     <?php include "../scripts/js.html"; ?>
     <?php include "../scripts/js-home.html"; ?>
+
+
 </body>
 
 </html>
